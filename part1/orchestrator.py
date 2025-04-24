e887045336e050b6c5483812399bc4c3b61f96a4
Alice <u1275258@utah.edu>
2025-04-23 23:29:50 -0600

    separate out files

--- part1/orchestrator.py
+++ part1/orchestrator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-import os
-#tells whether traffic is on north (n) or south (s) path
-curr_direction = "n"
+import subprocess
+
 
 def moveFlow(direction, curr_direction):
     if direction == curr_direction:
@@ -18,67 +17,10 @@ def moveFlow(direction, curr_direction):
 
 #not doing these in a for loop because the networks are confusing between routers
 #r1
-os.system("docker exec part1-r1-1 apt -y install curl")
-os.system("docker exec part1-r1-1 apt -y install gnupg")
-os.system("docker exec part1-r1-1 curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null")
-os.system("docker exec part1-r1-1 apt install lsb-release")
-os.system('docker exec part1-r1-1 FRRVER="frr-stable"')
-os.system("docker exec part1-r1-1 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list")
-os.system("docker exec part1-r1-1 apt update && apt -y install frr frr-pythontools")
-os.system("docker cp daemons part1-r1-1/etc/frr/")
-os.system("docker exec part1-r1-1 service frr restart")
-os.system("docker exec part1-r1-1 vtysh -c 'configure terminal' -c 'router ospf' -c 'network 10.0.14.0/24 area 0.0.0.0' -c 'network 10.0.16.0/24 area 0.0.0.0' -c 'network 10.0.18.0/24 area 0.0.0.0' -c 'end'")
-os.system("docker exec part1-r1-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-os.system("docker exec part1-r1-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-os.system("docker exec part1-r1-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
+subprocess.run(["./orchestrator", ""])
 
-#r2
-os.system("docker exec part1-r2-1 apt -y install curl")
-os.system("docker exec part1-r2-1 apt -y install gnupg")
-os.system("docker exec part1-r2-1 curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null")
-os.system("docker exec part1-r2-1 apt install lsb-release")
-os.system('docker exec part1-r2-1 FRRVER="frr-stable"')
-os.system("docker exec part1-r2-1 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list")
-os.system("docker exec part1-r2-1 apt update && apt -y install frr frr-pythontools")
-os.system("docker cp daemons part1-r2-1/etc/frr/")
-os.system("docker exec part1-r2-1 service frr restart")
-os.system("docker exec part1-r2-1 vtysh -c 'configure terminal' -c 'router ospf' -c 'network 10.0.16.0/24 area 0.0.0.0' -c 'network 10.0.17.0/24 area 0.0.0.0' -c 'end'")
-os.system("docker exec part1-r2-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-os.system("docker exec part1-r2-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-
-#r3
-os.system("docker exec part1-r3-1 apt -y install curl")
-os.system("docker exec part1-r3-1 apt -y install gnupg")
-os.system("docker exec part1-r3-1 curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null")
-os.system("docker exec part1-r3-1 apt install lsb-release")
-os.system('docker exec part1-r3-1 FRRVER="frr-stable"')
-os.system("docker exec part1-r3-1 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list")
-os.system("docker exec part1-r3-1 apt update && apt -y install frr frr-pythontools")
-os.system("docker cp daemons part1-r3-1/etc/frr/")
-os.system("docker exec part1-r3-1 service frr restart")
-os.system("docker exec part1-r3-1 vtysh -c 'configure terminal' -c 'router ospf' -c 'network 10.0.15.0/24 area 0.0.0.0' -c 'network 10.0.18.0/24 area 0.0.0.0' -c 'network 10.0.19.0/24 area 0.0.0.0' -c 'end'")
-os.system("docker exec part1-r3-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-os.system("docker exec part1-r3-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-os.system("docker exec part1-r3-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-
-#r4
-os.system("docker exec part1-r4-1 apt -y install curl")
-os.system("docker exec part1-r4-1 apt -y install gnupg")
-os.system("docker exec part1-r4-1 curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null")
-os.system("docker exec part1-r4-1 apt install lsb-release")
-os.system('docker exec part1-r4-1 FRRVER="frr-stable"')
-os.system("docker exec part1-r4-1 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list")
-os.system("docker exec part1-r4-1 apt update && apt -y install frr frr-pythontools")
-os.system("docker cp daemons part1-r4-1/etc/frr/")
-os.system("docker exec part1-r4-1 service frr restart")
-os.system("docker exec part1-r4-1 vtysh -c 'configure terminal' -c 'router ospf' -c 'network 10.0.16.0/24 area 0.0.0.0' -c 'network 10.0.17.0/24 area 0.0.0.0' -c 'end'")
-os.system("docker exec part1-r4-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 10' -c 'exit' -c 'end'")
-os.system("docker exec part1-r4-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 5' -c 'exit' -c 'end'")
-
-#connect endpoints
-os.system("docker exec part1-ha-1 route add -net 10.0.15.0/24 gw 10.0.14.10")
-os.system("docker exec part1-hb-1 route add -net 10.0.14.0/24 gw 10.0.15.3")
-
+#tells whether traffic is on north (n) or south (s) path
+curr_direction = "n"
 keepGoing = True
 while keepGoing:
     command = input("To change traffic flow path, type 'n' for north path and 's' for south path. Type 'exit' to stop the program.")
