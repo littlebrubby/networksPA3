86d637aa9d6cdd119628bb9b99454070acfda596
Alice <u1275258@utah.edu>
2025-04-24 00:38:29 -0600

    oof

--- part1/orchestrator.py
+++ part1/orchestrator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import os
 
 
 def moveFlow(direction, curr_direction):
@@ -8,16 +9,10 @@ def moveFlow(direction, curr_direction):
     if direction == "n":
         os.system("docker exec part1-r2-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 5' -c 'end'")
         os.system("docker exec part1-r4-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 10' -c 'end'")
-        curr_direction = "n"
     
     if direction == "s":
         os.system("docker exec part1-r4-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 5' -c 'end'")
         os.system("docker exec part1-r2-1 vtysh -c 'configure terminal' -c 'interface eth0' -c 'ip ospf cost 10' -c 'end'")
-        curr_direction = "s"
-
-#not doing these in a for loop because the networks are confusing between routers
-#r1
-subprocess.run(["./orchestrator", ""])
 
 #tells whether traffic is on north (n) or south (s) path
 curr_direction = "n"
@@ -26,6 +21,7 @@ while keepGoing:
     command = input("To change traffic flow path, type 'n' for north path and 's' for south path. Type 'exit' to stop the program.")
     if command == 'n' or command == 's':
         moveFlow(command, curr_direction)
+        curr_direction = command
         print("current direction:", curr_direction)
     elif command == 'exit':
         keepGoing = False
